19d65560830c6c55d8c0e39cb46ff6c6dabbbc2c
Jay <wilkinson_929@hotmail.com>
2018-02-19 21:35:16 +0000

    Vehicle blip settings should only update when necessary

--- resources/[LasVenturasPlayground]/vehicleblips/server/vehicleBlips.lua
+++ resources/[LasVenturasPlayground]/vehicleblips/server/vehicleBlips.lua
@@ -1,8 +1,17 @@
 function updateVehicleBlipRadius()
+	local element
+	if client then element = client else element = root end
 	outputDebugString("Updating vehicle blip radius to " ..tostring(get("blipStreamRadius")))
-	triggerClientEvent("onClientReceiveBlipRadiusInformation", resourceRoot, get("blipStreamRadius"))
+	triggerClientEvent("onClientReceiveBlipRadiusInformation", element, get("blipStreamRadius"))
 end
+
+addEventHandler("onSettingChange", resourceRoot, 
+	function(theSetting)
+		if(theSetting == "*" .. getResourceName(resource) .. ".blipStreamRadius") then
+			updateVehicleBlipRadius()
+		end 
+	end
+)
 
 addEvent("onClientRequestVehicleBlipRadiusValue", true)
 addEventHandler("onClientRequestVehicleBlipRadiusValue", resourceRoot, updateVehicleBlipRadius)
-addEventHandler("onSettingChange", resourceRoot, updateVehicleBlipRadius)
